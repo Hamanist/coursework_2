874bdba9b3d7c7ee91dceb28970bfbfde475d2c4
Hamanist <hamanist@gmail.com>
2023-10-25 17:59:37 +0300

    function editing

--- request_json/utils.py
+++ request_json/utils.py
@@ -2,7 +2,6 @@ import json
 import random
 import requests
 import urllib3
-
 from classes_for_the_game.basic_word import BasicWord
 
 urllib3.disable_warnings()
@@ -10,17 +9,13 @@ urllib3.disable_warnings()
 LINK = 'https://jsonkeeper.com/b/MZO8'
 
 
-def load_random_word(link):
+def load_random_word():
+    """
+    Принимает с внешнего ресурса файл json
+    :return: экземпляр класса BasicWord перемешивая слова
+    """
     response = requests.get(LINK, verify=False)
     file_json = json.loads(response.text)
     random.shuffle(file_json)
-    questions = []
-    for data in file_json:
-        word = data['word']
-        answer = data['subwords']
-        basic_word = BasicWord(word, answer)
-        questions.append(basic_word)
-    return questions
-
-
-print(load_random_word(LINK))
+    basic_word = BasicWord(file_json[0]["word"], file_json[0]["subwords"])
+    return basic_word
